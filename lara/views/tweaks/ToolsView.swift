c220fc304fb8dfc87237a8a1f1959fcf07a0cb6d
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-06-06 22:13:51 +0530

    Update ToolsView.swift

--- lara/views/tweaks/ToolsView.swift
+++ lara/views/tweaks/ToolsView.swift
@@ -1,10 +1,3 @@
-//
-//  ToolsView.swift
-//  lara
-//
-//  Created by ruter on 04.04.26.
-//
-
 import SwiftUI
 
 struct procentry: Identifiable, Hashable {
@@ -37,380 +30,188 @@ struct ToolsView: View {
 
         var label: String {
             switch self {
-            case .read:
-                return "read"
-
-            case .write:
-                return "write"
-
-            case .rw:
-                return "read-write"
+            case .read: return "read"
+            case .write: return "write"
+            case .rw: return "read-write"
             }
         }
     }
 
     var body: some View {
-        List {
+        NavigationStack {
+            List {
 
-            if !mgr.dsready {
+                if !mgr.dsready {
+                    Section {
+                        Text("Kernel R/W is not ready. Run the exploit first.")
+                            .foregroundColor(.secondary)
+                    } header: {
+                        Text("Status")
+                    }
+                }
+
                 Section {
-                    Text("Kernel R/W is not ready. Run the exploit first.")
-                        .foregroundColor(.secondary)
+                    HStack {
+                        Text("ASLR:")
 
-                } header: {
-                    Text("Status")
-                }
-            }
+                        Spacer()
 
-            Section {
-
-                HStack {
-                    Text("ASLR:")
-
-                    Spacer()
-
-                    Text(isaslr ? "enabled" : "disabled")
-                        .foregroundColor(
-                            isaslr ? Color.red : Color.green
-                        )
-                        .monospaced()
-
-                    Button {
-                        isaslr = aslrstate
-
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
-                    }
-                }
-
-                Button {
-                    toggleaslr()
-                    isaslr = aslrstate
-
-                } label: {
-                    Text("Toggle ASLR")
-                }
-
-            } header: {
-                Text("ASLR")
-
-            } footer: {
-                Text("Address Space Layout Randomization. Probably not useful for you.")
-            }
-
-            Section {
-
-                Button("Respring") {
-                    mgr.respring()
-                }
-
-                HStack {
-                    Text("ourproc:")
-
-                    Spacer()
-
-                    Text(
-                        mgr.dsready
-                        ? String(format: "0x%llx", ds_get_our_proc())
-                        : "N/A"
-                    )
-                    .foregroundColor(.secondary)
-                    .monospaced()
-                }
-
-                HStack {
-                    Text("ourtask:")
-
-                    Spacer()
-
-                    Text(
-                        mgr.dsready
-                        ? String(format: "0x%llx", ds_get_our_task())
-                        : "N/A"
-                    )
-                    .foregroundColor(.secondary)
-                    .monospaced()
-                }
-
-                HStack {
-                    Text("UID:")
-
-                    Spacer()
-
-                    Text("\(uid)")
-                        .font(.system(.body, design: .monospaced))
-                        .foregroundColor(.secondary)
-
-                    Button {
-                        uid = getuid()
-                        print(uid)
-
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
-                    }
-                }
-
-                HStack {
-                    Text("PID:")
-
-                    Spacer()
-
-                    Text("\(pid)")
-                        .font(.system(.body, design: .monospaced))
-                        .foregroundColor(.secondary)
-
-                    Button {
-                        pid = getpid()
-                        print(pid)
-
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
-                    }
-                }
-
-            } header: {
-                Text("Process")
-            }
-
-            // MARK: - Task Manager
-
-            Section {
-
-                HStack {
-                    Text("Process:")
-
-                    Spacer()
-
-                    TextField(
-                        "e.g. SpringBoard",
-                        text: $crashname
-                    )
-                    .textInputAutocapitalization(.never)
-                    .autocorrectionDisabled()
-                    .foregroundColor(.secondary)
-                    .monospaced()
-                    .fixedSize(
-                        horizontal: true,
-                        vertical: false
-                    )
-                }
-
-                Button("Crash") {
-                    crashname.withCString { cstr in
-                        _ = crashproc(cstr)
-                    }
-                }
-                .disabled(crashname.isEmpty)
-
-                Button("Pause") {
-                    crashname.withCString { cstr in
-                        _ = proc_pause_resume(cstr, false)
-                    }
-
-                    pausedProcesses.insert(crashname)
-                }
-                .disabled(
-                    crashname.isEmpty ||
-                    pausedProcesses.contains(crashname)
-                )
-
-                Button("Resume") {
-                    crashname.withCString { cstr in
-                        _ = proc_pause_resume(cstr, true)
-                    }
-
-                    pausedProcesses.remove(crashname)
-                }
-                .disabled(
-                    crashname.isEmpty ||
-                    !pausedProcesses.contains(crashname)
-                )
-                Button("SBX Escape Helper") {
-                    crashname.withCString { cstr in
-                        proc_sbx = procbyname(cstr)
-                    }
-                    if (proc_sbx == 0) {
-                        status = "Failed to get proc"
-                    }
-                    let errorcheck = sbx_escape(proc_sbx)
-                    if (errorcheck == 0) {
-                        return
-                    } else {
-                        status = "Failure!   - Steven He"
-                    }
-                }
-                .disabled(crashname.isEmpty)
-
-            } header: {
-                Text("Task Manager")
-
-            } footer: {
-                Text("Pause, Resume or Crash the selected process")
-            }
-
-            Section {
-
-                Button {
-
-                    if mgr.PPHelper() {
-                        status = """
-                        Succeeded. Open the Pocket Poster app, \
-                        open settings and tap Detect.
-                        """
-
-                    } else {
-                        status = "Failed. Check logs."
-                    }
-
-                } label: {
-                    Text("Pocket Poster Helper")
-                }
-                .disabled(!mgr.sbxready)
-
-            } header: {
-                Text("Pocket Poster")
-
-            } footer: {
-                Text(
-                    """
-                    Get the needed hashes for Pocket Poster \
-                    without the need of a PC.
-                    """
-                )
-            }
-
-            Section {
-
-                HStack {
-
-                    if showtoken {
-
-                        Text(
-                            mgr.sbxready
-                            ? "tkn"
-                            : "No Saved Token."
-                        )
-                        .foregroundColor(.secondary)
-                        .monospaced()
-
-                    } else {
-
-                        if !token.isEmpty {
-
-                            Text(token)
-                                .foregroundColor(.secondary)
-                                .monospaced()
-                                .lineLimit(1)
-                                .truncationMode(.middle)
-
-                        } else {
-
-                            Text("No Saved Token.")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-
-                    Spacer()
-
-                    Button {
-                        UIPasteboard.general.string =
-                            token.isEmpty ? nil : token
-
-                    } label: {
-                        Image(systemName: "doc.on.doc")
-                    }
-                    .disabled(token.isEmpty)
-                }
-                .contextMenu {
-
-                    if !token.isEmpty {
+                        Text(isaslr ? "enabled" : "disabled")
+                            .foregroundColor(isaslr ? .red : .green)
+                            .monospaced()
 
                         Button {
-                            UIPasteboard.general.string = token
-
+                            isaslr = aslrstate
                         } label: {
-                            Label(
-                                "Copy",
-                                systemImage: "doc.on.doc"
-                            )
+                            Image(systemName: "arrow.clockwise")
                         }
                     }
-                }
 
-                HStack {
-
-                    Text("Class:")
-
-                    Spacer()
-
-                    Picker(" ", selection: $issueclass) {
-
-                        ForEach(tokenclass.allCases) {
-                            tokenClass in
-
-                            Text(tokenClass.label)
-                                .tag(tokenClass)
-                        }
+                    Button("Toggle ASLR") {
+                        toggleaslr()
+                        isaslr = aslrstate
                     }
-                    .pickerStyle(.menu)
+                } header: {
+                    Text("ASLR")
                 }
 
-                HStack {
+                Section {
+                    Button("Respring") {
+                        mgr.respring()
+                    }
 
-                    Text("Path:")
-
-                    Spacer()
-
-                    TextField("/", text: $issuepath)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                        .foregroundColor(.secondary)
-                        .monospaced()
-                        .lineLimit(1)
-                        .truncationMode(.middle)
-                        .fixedSize(
-                            horizontal: true,
-                            vertical: false
+                    HStack {
+                        Text("ourproc:")
+                        Spacer()
+                        Text(
+                            mgr.dsready
+                            ? String(format: "0x%llx", ds_get_our_proc())
+                            : "N/A"
                         )
+                        .monospaced()
+                        .foregroundColor(.secondary)
+                    }
+
+                    HStack {
+                        Text("ourtask:")
+                        Spacer()
+                        Text(
+                            mgr.dsready
+                            ? String(format: "0x%llx", ds_get_our_task())
+                            : "N/A"
+                        )
+                        .monospaced()
+                        .foregroundColor(.secondary)
+                    }
+
+                    HStack {
+                        Text("UID:")
+                        Spacer()
+                        Text("\(uid)")
+                            .monospaced()
+                            .foregroundColor(.secondary)
+
+                        Button {
+                            uid = getuid()
+                        } label: {
+                            Image(systemName: "arrow.clockwise")
+                        }
+                    }
+
+                    HStack {
+                        Text("PID:")
+                        Spacer()
+                        Text("\(pid)")
+                            .monospaced()
+                            .foregroundColor(.secondary)
+
+                        Button {
+                            pid = getpid()
+                        } label: {
+                            Image(systemName: "arrow.clockwise")
+                        }
+                    }
+                } header: {
+                    Text("Process")
                 }
 
-                Button {
-                    token =
-                        mgr.sbxissuetoken(
-                            extClass: issueclass.rawValue,
-                            path: issuepath
-                        ) ?? ""
+                Section {
+                    HStack {
+                        Text("Process:")
+                        Spacer()
+                        TextField("e.g. SpringBoard", text: $crashname)
+                            .textInputAutocapitalization(.never)
+                            .autocorrectionDisabled()
+                            .monospaced()
+                            .fixedSize(horizontal: true, vertical: false)
+                    }
 
-                } label: {
-                    Text("Issue Token")
+                    Button("Crash") {
+                        crashname.withCString { _ = crashproc($0) }
+                    }
+                    .disabled(crashname.isEmpty)
+
+                    Button("Pause") {
+                        crashname.withCString { _ = proc_pause_resume($0, false) }
+                        pausedProcesses.insert(crashname)
+                    }
+                    .disabled(crashname.isEmpty || pausedProcesses.contains(crashname))
+
+                    Button("Resume") {
+                        crashname.withCString { _ = proc_pause_resume($0, true) }
+                        pausedProcesses.remove(crashname)
+                    }
+                    .disabled(crashname.isEmpty || !pausedProcesses.contains(crashname))
+
+                    Button("SBX Escape Helper") {
+                        crashname.withCString { cstr in
+                            proc_sbx = procbyname(cstr)
+                        }
+
+                        if proc_sbx == 0 {
+                            status = "Failed to get proc"
+                        }
+
+                        let errorcheck = sbx_escape(proc_sbx)
+                        status = errorcheck == 0 ? nil : "Failure!"
+                    }
+                    .disabled(crashname.isEmpty)
+                } header: {
+                    Text("Task Manager")
                 }
-                .disabled(!mgr.sbxready)
 
-            } header: {
-                Text("Sandbox")
+                Section {
+                    Button("Pocket Poster Helper") {
+                        status = mgr.PPHelper()
+                            ? "Succeeded. Open Pocket Poster settings."
+                            : "Failed. Check logs."
+                    }
+                    .disabled(!mgr.sbxready)
+                }
+
+                Section {
+                    Text(status ?? "No status")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .navigationTitle("Tools")
+
+            .scrollContentBackground(.hidden)
+            .background(Color(.systemBackground))
+            .ignoresSafeArea(edges: .bottom)
+
+            .alert("Status", isPresented: .constant(status != nil)) {
+                Button("OK") { status = nil }
+            } message: {
+                Text(status ?? "")
+            }
+
+            .onAppear {
+                if mgr.dsready {
+                    getaslrstate()
+                    isaslr = aslrstate
+                }
             }
         }
-        .navigationTitle("Tools")
-
-        .alert(
-            "Status",
-            isPresented: .constant(status != nil)
-        ) {
-            Button("OK") {
-                status = nil
-            }
-
-        } message: {
-            Text(status ?? "")
-        }
-
-        .onAppear {
-
-            if mgr.dsready {
-                getaslrstate()
-                isaslr = aslrstate
-            }
-        }
-        .ignoresSafeArea()
     }
 }
